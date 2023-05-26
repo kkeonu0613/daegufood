963777666cdca7a8cbb80700deb47f35a07c5df4
seonghyeon1022 <129020554+seonghyeon1022@users.noreply.github.com>
2023-05-27 01:46:58 +0900

    지도 중복 생성 수정

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-const toogleBtn = document.querySelector('.navbar__toogle');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
-
 const priceSort = document.querySelector('.container__price');
 const menuSort = document.querySelector('.container__menu');
 const placeSort = document.querySelector('.container__place');
-const randomSort = document.querySelector('container__random');
+const randomSort = document.querySelector('.container__random');
 
 const price = document.querySelector('#price');
-const block = document.querySelector('#menu');
+const menu = document.querySelector('#menu');
 const place = document.querySelector('#place');
 const random = document.querySelector('#random');
-
-toogleBtn.addEventListener('click', function () {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
 
 price.addEventListener('click', function () {
     priceSort.classList.add('active');
     menuSort.classList.remove('active');
     placeSort.classList.remove('active');
     randomSort.classList.remove('active');
-    
+    hideMap();
 });
 
-block.addEventListener('click', function () {
+menu.addEventListener('click', function () {
     priceSort.classList.remove('active');
     menuSort.classList.add('active');
     placeSort.classList.remove('active');
     randomSort.classList.remove('active');
+    hideMap();
 });
 
 place.addEventListener('click', function () {
@@ -37,6 +29,7 @@ place.addEventListener('click', function () {
     menuSort.classList.remove('active');
     placeSort.classList.add('active');
     randomSort.classList.remove('active');
+    hideMap();
 });
 
 random.addEventListener('click', function () {
@@ -44,6 +37,19 @@ random.addEventListener('click', function () {
     menuSort.classList.remove('active');
     placeSort.classList.remove('active');
     randomSort.classList.add('active');
-
+    showMap();
 });
 
+function hideMap() {
+    const mapContainer = document.getElementById('map-container');
+    mapContainer.style.display = 'none';
+}
+
+function showMap() {
+    const mapContainer = document.getElementById('map-container');
+    mapContainer.style.display = 'block';
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    hideMap();
+});
